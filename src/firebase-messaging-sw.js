c5d8f92caf475f079b7e2e3c07dd3c01d5de54e2
unknown <vickyscab24@gmail.com>
2024-04-28 14:58:38 +0530

    firebase init login deploy with emulators realtime database store-notify-default-rtdb.asia-southeast1.firebasedatabase.app (default)

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -2,7 +2,7 @@ importScripts('https://www.gstatic.com/firebasejs/9.1.3/firebase-app-compat.js')
 importScripts('https://www.gstatic.com/firebasejs/9.1.3/firebase-messaging-compat.js');
 
 
-firebase.initializeApp({
+/*firebase.initializeApp({
   apiKey: "AIzaSyCytNeMKRFnIa5tcTwC6ebe7Q6IbHJCfvQ",
   authDomain: "notification-40071.firebaseapp.com",
   projectId: "notification-40071",
@@ -11,5 +11,20 @@ firebase.initializeApp({
   appId: "1:737631106887:web:e8f5466bb2173eef723e62",
   measurementId: "G-V1E0Q3J4BY"
 });
+*/
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+firebase.initializeApp =( {
+  apiKey: "AIzaSyB-Z7TjtYw3N1Ks2vgx-um5nno9HSug2u0",
+  authDomain: "store-notify.firebaseapp.com",
+  projectId: "store-notify",
+  storageBucket: "store-notify.appspot.com",
+  messagingSenderId: "396732087280",
+  appId: "1:396732087280:web:08481e982ba04e72043b35",
+  measurementId: "G-FFGVK38GZK"
+});
+
+
+
 const messaging = firebase.messaging();
 
